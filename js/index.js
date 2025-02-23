17b12d41ff05ba7af95d1472fc5752aa6b256b4d
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2025-02-23 14:58:53 +0200

    Update index js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 function init() {
-  import('./header.js');
-  import('./index.hero.js');
-  import('./index.popular-items.js');
-  import('./index.categories.js');
-  import('./index.countdown.js');
-  import('./index.swiper.js');
-  import('./index.news.js');
-  import('./scroll-to-top.js');
-  import('./global.cart.js');
-  import('./index.footer-contact-form.js');
-  import('./shop.cat.js');
-  import('./shop.category.js');
-  import('./footer.js');
+  import("./header.js");
+  import("./index.hero.js");
+  import("./index.popular-items.js");
+  import("./index.categories.js");
+  import("./index.countdown.js");
+  import("./index.swiper.js");
+  import("./index.news.js");
+  import("./scroll-to-top.js");
+  import("./global.cart.js");
+  import("./shop.cat.js");
+  import("./shop.category.js");
+  import("./index.footer-contact-form.js");
+  import("./footer.js");
 }
 
 const totalPartials = document.querySelectorAll(
@@ -19,7 +19,7 @@ const totalPartials = document.querySelectorAll(
 ).length;
 let loadedPartialsCount = 0;
 
-document.body.addEventListener('htmx:afterOnLoad', () => {
+document.body.addEventListener("htmx:afterOnLoad", () => {
   loadedPartialsCount++;
   if (loadedPartialsCount === totalPartials) init();
 });
