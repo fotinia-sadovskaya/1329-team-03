4edd46ad41f1f2947a27cdbe1d2733a5f7b7a0e3
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2025-02-23 14:49:15 +0200

    Update index js

--- js/index.js
+++ js/index.js
@@ -2,12 +2,16 @@ function init() {
   import('./header.js');
   import('./index.hero.js');
   import('./index.popular-items.js');
-  import('./index.countdown.js');
-  import('./footer.js');
   import('./index.categories.js');
+  import('./index.countdown.js');
+  import('./index.swiper.js');
   import('./index.news.js');
   import('./scroll-to-top.js');
-  import('./index.swiper.js');
+  import('./global.cart.js');
+  import('./index.footer-contact-form.js');
+  import('./shop.cat.js');
+  import('./shop.category.js');
+  import('./footer.js');
 }
 
 const totalPartials = document.querySelectorAll(
